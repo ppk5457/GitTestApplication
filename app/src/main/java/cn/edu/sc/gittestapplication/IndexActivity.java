f3198799f16644e080716db51b51fb6aa78b452e
ppk5457 <developer013@163.com>
2020-06-15 09:26:54 +0800

    Add indexActivity file

---
+++ app/src/main/java/cn/edu/sc/gittestapplication/IndexActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.sc.gittestapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
