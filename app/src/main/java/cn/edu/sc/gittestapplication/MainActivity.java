832e56e02e328929d40f41a14e3585f3725ee636
ppk5457 <developer013@163.com>
2020-06-15 09:29:52 +0800

    Add indexActivity file

--- app/src/main/java/cn/edu/sc/gittestapplication/MainActivity.java
+++ app/src/main/java/cn/edu/sc/gittestapplication/MainActivity.java
@@ -3,12 +3,22 @@ package cn.edu.sc.gittestapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtHello=findViewById(R.id.txtHello);
+        txtHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
